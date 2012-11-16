187ba0524564a0cab70acf78267a654d0ad1a313
Zac <zac@kleinpeter.org>
2012-11-16 13:23:52 -0600

    Adding Game object

--- lib/outpost/game.rb
+++ lib/outpost/game.rb
@@ -1,2 +1,18 @@
 class Outpost::Game
+  attr_accessor :history, :iteration
+
+  def initialize
+    @iteration = 1
+    @history = { @iteration => Outpost::Board.new }
+  end
+
+  def board
+    @history[@iteration]
+  end
+
+
+  def move piece, square
+    piece.square, square.piece = square, piece
+  end
+
 end
